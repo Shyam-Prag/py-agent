bfa2df8b9d0699da4a697eef27ba87a66baf1dfc
Shyam-Prag <pragshyam11@gmail.com>
2025-08-20 20:40:57 +0000

    added test cases

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,14 +1,19 @@
 import os 
 
 def get_files_info(working_directory, directory="."):
-    
+    # First checking working directory 
     working_directory_absolute_path = os.path.abspath(working_directory)
     print(f"working_directory_absolute_path: {working_directory_absolute_path}")
 
-    directory_absolute_path = os.path.join(working_directory_absolute_path,directory)
+    # check the inputted directory 
+    directory_absolute_path = os.path.join(working_directory_absolute_path, directory)
     print(f"directory_absolute_path: {directory_absolute_path}")
+
+    #handle directory absolute path when "../" inputs are provided. 
+    directory_abs_path = os.path.abspath(directory_absolute_path)
+    print(f"directory_abs_path: {directory_abs_path}")
     
-    if not directory_absolute_path.startswith(working_directory_absolute_path):
+    if not directory_abs_path.startswith(working_directory_absolute_path):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
     if not os.path.isdir(directory_absolute_path):
@@ -23,6 +28,8 @@ def get_files_info(working_directory, directory="."):
         files = os.listdir(directory_absolute_path)
 
         for file in files:
+            if file == "__pycache__":
+                continue 
             print(f"file: {file}")
             file_size = os.path.getsize(os.path.join(directory_absolute_path,file))
             is_dir = os.path.isdir(os.path.join(directory_absolute_path,file))
--- tests.py
+++ tests.py
@@ -6,14 +6,52 @@ class TestGetFile(unittest.TestCase):
         result = get_files_info("calculator", ".")
         print("--- Actual Result ---")
         print(result)
-        print("--- Expected Result ---")
 
         expected_result = """Result for current directory:
 - main.py: file_size=564 bytes, is_dir=False
 - tests.py: file_size=1330 bytes, is_dir=False
 - pkg: file_size=63 bytes, is_dir=True
-"""
+"""     
+        print("--- Expected Result ---")
+        print(expected_result)
         assert result == expected_result
+    
+    def test_get_file_info_pck_directory(self):
+        result = get_files_info("calculator","pkg")
+        print("--- Actual Result ---")
+        print(result)
+        
+
+        expected_result = """Result for 'pkg' directory:
+- calculator.py: file_size=1738 bytes, is_dir=False
+- render.py: file_size=767 bytes, is_dir=False
+"""
+        print("--- Expected Result ---")
+        print(expected_result)
+        assert result == expected_result
+    
+    def test_non_existing_path(self):
+        result = get_files_info("calculator", "/bin")
+        print("--- Actual Result ---")
+        print(result)
+
+        expected_result = 'Error: Cannot list "/bin" as it is outside the permitted working directory'
+        print("--- Expected Result ---")
+        print(expected_result)
+
+        assert result == expected_result
+    
+    def test_non_permitted_directory(self):
+        result = get_files_info("calculator", "../")
+        print("--- Actual Result ---")
+        print(result)
+
+        expected_result = 'Error: Cannot list "../" as it is outside the permitted working directory'
+        print("--- Expected Result ---")
+        print(expected_result)
+
+        assert result == expected_result
+
 
 if __name__ == '__main__':
     unittest.main()
